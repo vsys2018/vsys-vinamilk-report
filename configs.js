563458cbd7e0354ad92e2af364fa2d4d25fdcc15
lbtien1990 <lbtien1990@gmail.com>
2019-06-26 14:54:13 +0700

    Update

--- configs.js
+++ configs.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongouri: "mongodb://vsdemo:2018%24CongHoa19614@35.247.153.138:19614/demo",
-  collection1: "gd_naptien",
-  collection2: "gd_ruttien",
-  appport: 8002
+  collection1: "demo_sua",
+  collection2: "",
+  appport: 8004
 };
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get("/report/:code", (req, res) => {
   res.render("index.html", { data: req.params.code });
 });
 
-app.post("/api/naptien01", (req, res) => {
+app.post("/api/vinamilk", (req, res) => {
   MongoClient.connect(configs.mongouri, { useNewUrlParser: true }, (cnnError, client) => {
     if (cnnError) {
       res.json({ error: cnnError.message, type: "Connection Error" });
@@ -60,46 +60,7 @@ app.post("/api/naptien01", (req, res) => {
   });
 });
 
-app.post("/api/ruttien01", (req, res) => {
-  MongoClient.connect(configs.mongouri, { useNewUrlParser: true }, (cnnError, client) => {
-    if (cnnError) {
-      res.json({ error: cnnError.message, type: "Connection Error" });
-      return;
-    }
-    let db = client.db();
 
-    db.collection(configs.collection2, (colError, col) => {
-      if (colError) {
-        res.json({ error: cnnError.message, type: "Collection Error" });
-        return;
-      }
-      col.find(
-        {
-          t: {
-            $gte: new Date(req.body.fromdate),
-            $lt: new Date(req.body.todate)
-          },
-          $or: req.body.filter
-        },
-        { projection: { _id: 0, cnt: 0, id: 0, c8: 0 } },
-        (resError, cursor) => {
-          if (resError) {
-            res.json({ error: cnnError.message, type: "Find Error" });
-            return;
-          }
-          cursor.toArray((curError, docs) => {
-            if (curError) {
-              res.json({ error: cnnError.message, type: "Data Error" });
-              return;
-            }
-
-            res.json(docs);
-          });
-        }
-      );
-    });
-  });
-});
 
 // SERVER
 const server = Server(app);
